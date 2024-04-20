553ddc3350df0f3809f2e113011f988a4fd83ecf
23ijo30 <23ijo30@agh.pl>
2024-04-20 12:09:55 +0200

    Lab03 done

--- src/test/java/Lab_2_WebDriver_Test.java
+++ src/test/java/Lab_2_WebDriver_Test.java
@@ -12,36 +12,36 @@ import java.util.List;
 public class Lab_2_WebDriver_Test {
 
     @Test
-    public void playWithWebDriver (){
-        /*Arrange*/
+    public void playWithWebDriver() {
+        //Arrange
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://demo.playwright.dev/todomvc");
-        /*Act*/
+        //Act
         WebElement toDoTxt = driver.findElement(By.cssSelector(".new-todo"));
         toDoTxt.sendKeys("Naprawić zlew" + Keys.ENTER);
-        /*Assert*/
+        //Assert
         WebElement expTodoTitleElm = driver.findElement(By.cssSelector("[data-testid=todo-title]"));
         Assert.assertEquals(expTodoTitleElm.getText(), "Naprawić zlew");
 
         WebElement countElm = driver.findElement(By.cssSelector("[data-testid=todo-count]"));
         Assert.assertEquals(countElm.getText(), "1 item left");
 
-        /*Act*/
-         WebElement todoItemChk = driver.findElement(By.cssSelector("[data-testid=todo-item] input"));
-         todoItemChk.click();
+        //Act
+        WebElement todoItemChk = driver.findElement(By.cssSelector("[data-testid=todo-item] input"));
+        todoItemChk.click();
         WebElement expTodoItemElm = driver.findElement(By.cssSelector("[data-testid=todo-item]"));
-        /*Assert*/
+        //Assert
         Assert.assertEquals(expTodoItemElm.getAttribute("class"), "completed");
         countElm = driver.findElement(By.cssSelector("[data-testid=todo-count]"));
         Assert.assertEquals(countElm.getText(), "0 items left");
 
-        /*Act*/
+        //Act
         WebElement todoItemDeleteBtn = driver.findElement(By.cssSelector("button.destroy"));
         todoItemDeleteBtn.click();
         List<WebElement> expTodoItems = driver.findElements(By.cssSelector("[data-testid=todo-item"));
-        /*Assert*/
+        //Assert
         Assert.assertEquals(expTodoItems.size(), 0);
 
     }
